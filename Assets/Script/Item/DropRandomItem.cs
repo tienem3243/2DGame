d686263d6582c627ac01632fa165ff5118e251b4
Tăng Chí Chung <52872325+HiamKaito@users.noreply.github.com>
2022-04-08 22:40:45 +0700

    Update droprandom item

--- Assets/Script/Item/DropRandomItem.cs
+++ Assets/Script/Item/DropRandomItem.cs
@@ -4,46 +4,41 @@ using UnityEngine;
 
 public class DropRandomItem : MonoBehaviour
 {
-    // public static DropRandomItem _instance;
     public GameObject[] _prefab;
-    // public int[] _prefabCount;
-    /*
-    private void Start()
+    public int[] _prefabChance;
+
+    public GameObject SelectItem()
     {
-        _instance = this;
+        // Calculate the summa of all portions.
+        int poolSize = 0;
+        for (int i = 0; i < _prefab.Length; i++)
+        {
+            // poolSize += items[i].chance;
+            poolSize += _prefabChance[i];
+        }
+
+        // Get a random integer from 0 to PoolSize.
+        // int randomNumber = rnd.Next(0, poolSize) + 1;
+        int randomNumber = Random.Range(0, poolSize) + 1;
+
+        // Detect the item, which corresponds to current random number.
+        int accumulatedProbability = 0;
+        for (int i = 0; i < _prefab.Length; i++)
+        {
+            // accumulatedProbability += items[i].chance;
+            accumulatedProbability += _prefabChance[i];
+            if (randomNumber <= accumulatedProbability)
+                return _prefab[i];
+        }
+        return null;    // this code will never come while you use this programm right :)
     }
-    */
+
 
     public void dropRandomItem()
     {
-        /*
-    i       1    2   3
-    obj     A    B   C
-    rate    50%  30% 20%
-        
-    Random (1 -> 3): 1, 2, 3
+        // int pos = Random.Range(0, _prefab.Length);
+        // Debug.Log("Drop item at pos " + pos + " has lenght : " + _prefab.Length);
 
-    Create List manager all prefab, and mixing it.
-    
-    For: loop all prefab
-        For: add all ID prefab with the prefabCount into list
-        */
-        // List<int> list = new List<int>();
-        // for (int i = 0; i < _prefabCount.Length; i++)
-        // {
-        //     for (int j = 1; j < _prefabCount[i]; j++)
-        //     {
-        //         list.Add(i);
-        //     }
-        // }
-
-        // Random.Range(0, )
-        // list.random
-
-
-        int pos = Random.Range(0, _prefab.Length);
-        Debug.Log("Drop item at pos " + pos + " has lenght : " + _prefab.Length);
-
-        Instantiate(_prefab[pos], gameObject.GetComponentInParent<Transform>().position, Quaternion.identity);
+        Instantiate(SelectItem(), gameObject.GetComponentInParent<Transform>().position, Quaternion.identity);
     }
 }
