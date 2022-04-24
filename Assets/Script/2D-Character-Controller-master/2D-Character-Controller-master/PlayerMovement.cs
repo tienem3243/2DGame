f4f9433174e79ee652f33e2bd243dae4b27b7a82
Just another Dev <60642132+prohzgod@users.noreply.github.com>
2022-04-24 20:25:40 +0700

    add pause menu @tienem3243

--- Assets/Script/2D-Character-Controller-master/2D-Character-Controller-master/PlayerMovement.cs
+++ Assets/Script/2D-Character-Controller-master/2D-Character-Controller-master/PlayerMovement.cs
@@ -3,77 +3,82 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
-	public CharacterController2D controller;
-	public Vector2 StartPos;//where you stand when scene change
-	public Animator animator;
-	public float runSpeed = 40f;
-	float horizontalMove = 0f;
-	bool jump = false;
-	bool dash = false;
-	bool crouch = false;
-	bool atk = false;
+    public CharacterController2D controller;
+    public Vector2 StartPos;//where you stand when scene change
+    public Animator animator;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool dash = false;
+    bool crouch = false;
+    bool atk = false;
 
     private void OnDrawGizmosSelected()
     {
-		Gizmos.DrawCube(StartPos,new Vector2(1, 1));
+        Gizmos.DrawCube(StartPos, new Vector2(1, 1));
     }
-	
-	
+
+
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
 
-		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
-		if (Input.GetButtonDown("Jump"))
-		{
-			jump = true;
-			animator.SetBool("IsJumping", true);
-		}
-		if (Input.GetButtonDown("Dash"))
-		{
-			dash = true;
-			animator.SetTrigger("IsDashing");
-		}
-		if (Input.GetButtonDown("Crouch"))
-		{
-			crouch = true;
-			Debug.Log(crouch);
-		}
-		else if (Input.GetButtonUp("Crouch"))
-		{
-			crouch = false;
-			Debug.Log(crouch);
-		}
-		if (Input.GetButtonDown("MeleeAtack"))
+
+
+        if (Input.GetButtonDown("Jump"))
         {
-			atk = true;
+            jump = true;
+            animator.SetBool("IsJumping", true);
         }
-	}
+        if (Input.GetButtonDown("Dash"))
+        {
+            dash = true;
+            animator.SetTrigger("IsDashing");
+        }
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+            Debug.Log(crouch);
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+            Debug.Log(crouch);
+        }
+        if (Input.GetButtonDown("MeleeAtack"))
+        {
+            atk = true;
+        }
 
-	public void OnLanding ()
-	{
-		animator.SetBool("IsJumping", false);
-		animator.SetBool("IsGround",true);
-	}
-	public void OnAir()
-	{
-		animator.SetBool("IsJumping", true);
-		animator.SetBool("IsGround", false);
-	}
-	public void OnCrouching(bool isCrouching)
-	{
-	}
+    }
 
-	void FixedUpdate ()
-	{
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+        animator.SetBool("IsGround", true);
+    }
+    public void OnAir()
+    {
+        animator.SetBool("IsJumping", true);
+        animator.SetBool("IsGround", false);
+    }
+    public void OnCrouching(bool isCrouching)
+    {
+    }
+
+    void FixedUpdate()
+    {
 
         controller.Move(horizontalMove * Time.fixedDeltaTime, jump, dash, crouch, atk);
-		dash = false;
-		jump = false;
-	}
+        dash = false;
+        jump = false;
+    }
 }
